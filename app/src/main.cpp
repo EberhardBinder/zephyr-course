3b605eea83e86bde2eacdaaac0996dd786825fff
Eberhard Binder <eberhard.binder@reutlingen-university.de>
2026-09-05 15:49:38 +0200

    Complete assignment work

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,9 +4,9 @@
 
 #define SLEEP_TIME_MS 1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+/* The devicetree node identifier for the "app-led" alias. */
 
+#define LED_NODE DT_ALIAS(app_led)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
@@ -21,10 +21,9 @@ int main(void)
 
     while (1) {
         if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
-        led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+        led_state = !led_state;
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
