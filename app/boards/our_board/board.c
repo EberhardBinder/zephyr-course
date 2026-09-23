232a100b2fe12cf6ba0a1634002134fbb75e39f7
Eberhard Binder <eberhard.binder@reutlingen-university.de>
2026-09-23 22:34:45 +0200

    Complete assignment work

--- app/boards/our_board/board.c
+++ app/boards/our_board/board.c
@@ -1,15 +1,13 @@
 #include<zephyr/init.h>
 #include<zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-
-LOG_MODULE_REGISTER(board_out_board_init, LOG_LEVEL_INF);
 
 static int board_our_board_init(void) {
 
 	/* Some function to run before boot */
 
-        LOG_INF("Board Initialized");
+        printk("Board Initialized");
 
 	return 0;
 }
 
+SYS_INIT(board_our_board_init, APPLICATION, 0);
